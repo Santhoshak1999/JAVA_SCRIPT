ed9d0b08eb98dd1d2649631d2a2e1ec0c596e2cf
SanthoshaK1999 <sk1416986@gmail.com>
2024-07-13 19:31:28 +0530

    Comparision Operator in JS

--- 04_Comparison.js
+++ 04_Comparison.js
@@ -7,7 +7,7 @@ console.log('' == 0);          // true, empty string is converted to number 0
 console.log([1,2] == '1,2');   // true, array [1,2] is converted to string '1,2'
 
 // Comparison using ===
-console.log("Comparison using ===");
+console.log("Comparison using ===(Strict Equality Check");
 console.log(5 === '5');        // false, different types (number vs string)
 console.log(0 === false);      // false, different types (number vs boolean)
 console.log(null === undefined); // false, different types (null vs undefined)
@@ -19,16 +19,16 @@ console.log("Comparison using >, <, >=, <=")
 console.log(5 > '4');          // true, '4' is converted to number 4
 console.log('12' < 20);        // true, '12' is converted to number 12
 console.log('2' >= 2);         // true, '2' is converted to number 2
-console.log('02' < 1);         // true, '02' is converted to number 2
+console.log('02' < 1);         // false, '02' is converted to number 2
 console.log('5' <= 5);         // true, '5' is converted to number 5
 
-// Special case with null
+// Special case with null-***
 console.log("Special case with null");
 console.log(null > 0);         // false, null is converted to 0, 0 > 0 is false
 console.log(null < 1);         // true, null is converted to 0, 0 < 1 is true
 console.log(null >= 0);        // true, null is converted to 0, 0 >= 0 is true
 console.log(null <= 0);        // true, null is converted to 0, 0 <= 0 is true
-console.log(null == 0);        // false, null is not loosely equal to 0
+console.log(null == 0);        // false, null is not converted to 0 and null is not loosely equal to 0
 console.log(null == undefined); // true, null is loosely equal to undefined
 
 // Explicit type conversion
