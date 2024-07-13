ee1ddc067b295150af55e7c28d38920b8b902002
SanthoshaK1999 <sk1416986@gmail.com>
2024-07-13 20:38:45 +0530

    Non primitive data types and reference data types in javaScript

---
+++ 02_02_RefferenceDataType.js
@@ -0,0 +1,16 @@
+//Reference - Non primitive data types
+//Array, Objects, Fuctions
+
+Colors = ["Green", "White", "Green", "Black"];
+myObj = {
+    name : "santhosha K",
+    age : 24,
+    city : "davangere" 
+};
+myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(Colors);
+console.log(myObj);
+myfunction();
