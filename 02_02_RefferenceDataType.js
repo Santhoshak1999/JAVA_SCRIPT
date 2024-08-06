30df0871e8a8be9350be22a4d35885f0df5f3c9f
SanthoshaK1999 <sk1416986@gmail.com>
2024-08-06 06:09:53 +0530

    Reference Data Types - Array, ArrayObject and Funtions

--- 02_02_RefferenceDataType.js
+++ 02_02_RefferenceDataType.js
@@ -1,16 +1,28 @@
 //Reference - Non primitive data types
 //Array, Objects, Fuctions
 
+
+//Array : use index to store the elements
 Colors = ["Green", "White", "Green", "Black"];
+
+//Array Object: used to store the properties - key:value pairs
 myObj = {
     name : "santhosha K",
     age : 24,
     city : "davangere" 
 };
+
+
 myfunction = function(){
     console.log("Hello World");
 }
 
+console.log(typeof Colors);
+console.log(typeof myObj);
+console.log(typeof myfunction);
+
+
 console.log(Colors);
 console.log(myObj);
+console.log(myObj.name)
 myfunction();
